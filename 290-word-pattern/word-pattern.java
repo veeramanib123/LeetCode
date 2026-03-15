f35a4b32d61359e54fe392268c1529709124bca3
veeramanib123 <veeramanisangaralingamb@gmail.com>
2026-03-15 18:53:06 +0530

    Time: 1 ms (82.35%) | Memory: 42.8 MB (51.71%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s)
+    {
+        String[] words=s.split(" ");
+        if(pattern.length() != words.length)
+        {
+            return false;
+        }
+        HashMap<Character,String> map=new HashMap<>();
+        HashSet<String> seen=new HashSet<>();
+        for(int i=0;i<pattern.length();i++)
+        {
+            char c=pattern.charAt(i);
+            String w=words[i];
+            if(map.containsKey(c))
+            {
+                if(!(map.get(c).equals(w)))
+                {
+                    return false;
+                }          
+            }
+            else
+            {
+                if(seen.contains(w))
+                {
+                    return false;
+                }
+                map.put(c,w);
+                seen.add(w);
+            }
+            
+        }
+        return true;
+    }
+}
