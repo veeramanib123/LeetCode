6b0ce18fc68faffbce245607005f881506f05035
veeramanib123 <veeramanisangaralingamb@gmail.com>
2026-04-14 13:44:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -2,10 +2,10 @@ class Solution {
     public boolean validMountainArray(int[] arr) {
         int n = arr.length;
         if(n < 3) return false;
-        int i = 0;
-        while(i + 1 < n && arr[i] < arr[i+1]) i++;
-        if(i == 0 || i == n - 1) return false;
-        while(i + 1 < n && arr[i] > arr[i+1]) i++;
-        return i == n - 1;
+        int i = 1;
+        while(i < n && arr[i-1] < arr[i]) i++;
+        if(i == 1 || i == n ) return false;
+        while(i + 1 < n && arr[i-1] > arr[i]) i++;
+        return i == n;
     }
 }
