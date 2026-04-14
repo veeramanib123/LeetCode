53078af1e439a9edabcb0e95213175902f13dd61
veeramanib123 <veeramanisangaralingamb@gmail.com>
2026-04-14 13:45:11 +0530

    Time: 1 ms (100%), Space: 47.5 MB (40.94%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -5,7 +5,7 @@ class Solution {
         int i = 1;
         while(i < n && arr[i-1] < arr[i]) i++;
         if(i == 1 || i == n ) return false;
-        while(i + 1 < n && arr[i-1] > arr[i]) i++;
+        while(i < n && arr[i-1] > arr[i]) i++;
         return i == n;
     }
 }
